7e70898740a818c569b1b8df42a92e2e6c99d3e1
jinii9 <69418829+jinii9@users.noreply.github.com>
2021-05-28 02:11:55 +0900

    인스타그램 어댑터

---
+++ app/src/main/java/gachon/mpclass/mp_team_newnew/MyItem.java
@@ -0,0 +1,19 @@
+package gachon.mpclass.mp_team_newnew;
+
+public class MyItem {
+    String title;
+
+    public MyItem(String title) {
+        this.title = title;
+    }
+
+    public MyItem() {}
+
+    public String getId() {
+        return title;
+    }
+
+    public void setId(String title) {
+        this.title = title;
+    }
+}
