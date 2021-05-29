a822d227cc3e145d483e297ffd37974b1d283bd4
jiyeon <youig8@naver.com>
2021-05-29 16:07:07 +0900

    다시 ok

--- app/src/main/java/gachon/mpclass/mp_team_newnew/form/TodaySaleForm.java
+++ app/src/main/java/gachon/mpclass/mp_team_newnew/form/TodaySaleForm.java
@@ -1,100 +1,30 @@
 package gachon.mpclass.mp_team_newnew.form;
 
 public class TodaySaleForm {
-    private String imgURL;
-    private String title;
-    private String description;
-    private String information;
-    private String ingredients_name;
-    private String ingredients_quantity;
-    private String anniversary;
-    private String country;
-    private String videoURL;
+    private String info;
+    private String address;
 
     @Override
     public String toString() {
         return "TodaySaleForm{" +
-                "imgURL='" + imgURL + '\'' +
-                ", title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", information='" + information + '\'' +
-                ", ingredients_name='" + ingredients_name + '\'' +
-                ", ingredients_quantity='" + ingredients_quantity + '\'' +
-                ", anniversary='" + anniversary + '\'' +
-                ", country='" + country + '\'' +
-                ", videoURL='" + videoURL + '\'' +
+                "information='" + info + '\'' +
+                ", address='" + address + '\'' +
                 '}';
     }
 
-    public String getImgURL() {
-        return imgURL;
+    public String getInfo() {
+        return info;
     }
 
-    public void setImgURL(String imgURL) {
-        this.imgURL = imgURL;
+    public void setInfo(String info) {
+        this.info = info;
     }
 
-    public String getTitle() {
-        return title;
+    public String getAddress() {
+        return address;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getInformation() {
-        return information;
-    }
-
-    public void setInformation(String information) {
-        this.information = information;
-    }
-
-    public String getIngredients_name() {
-        return ingredients_name;
-    }
-
-    public void setIngredients_name(String ingredients_name) {
-        this.ingredients_name = ingredients_name;
-    }
-
-    public String getIngredients_quantity() {
-        return ingredients_quantity;
-    }
-
-    public void setIngredients_quantity(String ingredients_quantity) {
-        this.ingredients_quantity = ingredients_quantity;
-    }
-
-    public String getAnniversary() {
-        return anniversary;
-    }
-
-    public void setAnniversary(String anniversary) {
-        this.anniversary = anniversary;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getVideoURL() {
-        return videoURL;
-    }
-
-    public void setVideoURL(String videoURL) {
-        this.videoURL = videoURL;
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
