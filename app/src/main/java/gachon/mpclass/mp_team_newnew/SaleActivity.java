a5d576d21f4c40aa0dbfacd5d3e077a969a2bc50
jinii9 <69418829+jinii9@users.noreply.github.com>
2021-05-29 14:53:50 +0900

    동네특가 어댑터하던거

--- app/src/main/java/gachon/mpclass/mp_team_newnew/SaleActivity.java
+++ app/src/main/java/gachon/mpclass/mp_team_newnew/SaleActivity.java
@@ -1,6 +1,8 @@
 package gachon.mpclass.mp_team_newnew;
 
 import android.os.Bundle;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,4 +13,11 @@ public class SaleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sale);
     }
 
+    //어댑터
+    ListView listView = findViewById(R.id.lv_comment_view);
+
+    class MyAdapter extends BaseAdapter {
+
+
+    }
 }
