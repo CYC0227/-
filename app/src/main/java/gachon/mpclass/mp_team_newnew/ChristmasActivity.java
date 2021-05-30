81670395a7420e2de51ae8bbebde898008e7bc00
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2021-05-30 23:05:05 +0900

    크리스마스가 오지않아...

--- app/src/main/java/gachon/mpclass/mp_team_newnew/ChristmasActivity.java
+++ app/src/main/java/gachon/mpclass/mp_team_newnew/ChristmasActivity.java
@@ -6,19 +6,13 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import org.w3c.dom.Text;
 
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -36,7 +30,11 @@ public class ChristmasActivity extends AppCompatActivity implements View.OnClick
     TextView edit_title;
     private ArrayList<PostingForm> data = null;
 
-    List<Bitmap> imgList = new ArrayList<>();
+    static List<Bitmap> imgList2 = new ArrayList<>();
+//    static List<PostingForm> postingFormList2 = new ArrayList<>();
+
+    //저장된 리스트
+    List<PostingForm> postingFormList = new ArrayList<>();
 
     RetrofitClient retrofitClient = new RetrofitClient();
     Call<List<PostingForm>> call;
@@ -50,14 +48,42 @@ public class ChristmasActivity extends AppCompatActivity implements View.OnClick
         String anniv  = "Christmas";
         call = retrofitClient.retrofitService.getPostsByAnniversary(anniv);
 
-        //저장된 리스트
-        List<PostingForm> postingFormList = new ArrayList<>();
 
         call.enqueue(new Callback<List<PostingForm>>() {
             @Override
             public void onResponse(Call<List<PostingForm>> call, Response<List<PostingForm>> response) {
                 for(PostingForm postingForm: response.body()) {
                     postingFormList.add(postingForm);
+
+                    System.out.println("postingForm = " + postingForm);
+                    System.out.println("@@@@@@@@@@@@@@##################@@@@@@@@@@@@@########");
+                }
+
+                for (PostingForm postingForm: postingFormList) {
+                    String img = postingForm.getImgURL();
+
+                    Call<ResponseBody> callImageDown;
+                    callImageDown = retrofitClient.retrofitService.downloadFile(img);
+
+                    callImageDown.enqueue(new Callback<ResponseBody>() {
+                        @Override
+                        public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                            InputStream is = response.body().byteStream();
+                            Bitmap bitmap = BitmapFactory.decodeStream(is);
+
+                            imgList2.add(bitmap);
+                            System.out.println("bitmap = " + bitmap);
+                            System.out.println("biiit");
+
+
+                        }
+
+                        @Override
+                        public void onFailure(Call<ResponseBody> call, Throwable t) {
+                            Log.d("TAG", "Image download error: " + t.getLocalizedMessage());
+
+                        }
+                    });
 
                 }
             }
@@ -66,43 +92,24 @@ public class ChristmasActivity extends AppCompatActivity implements View.OnClick
             public void onFailure(Call<List<PostingForm>> call, Throwable t) {
                 System.out.println(t.getMessage());
                 Log.d("tag4","실패" + t.getMessage());
+                System.out.println("FAIL");
+
             }
+
+
+
         });
 
 
 //image download
 
-        for (PostingForm postingForm: postingFormList) {
-            String img = postingForm.getImgURL();
-
-            Call<ResponseBody> callImageDown;
-            callImageDown = retrofitClient.retrofitService.downloadFile(img);
-
-            callImageDown.enqueue(new Callback<ResponseBody>() {
-                @Override
-                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                    InputStream is = response.body().byteStream();
-                    Bitmap bitmap = BitmapFactory.decodeStream(is);
-
-                    imgList.add(bitmap);
-                    System.out.println("bitmap = " + bitmap);
-                }
-
-                @Override
-                public void onFailure(Call<ResponseBody> call, Throwable t) {
-                    Log.d("TAG", "Image download error: " + t.getLocalizedMessage());
-
-                }
-            });
-
-        }
 
 
         ListView listView = (ListView) findViewById(R.id.listview);
 
 
 
-        MyrecipeAdapter adapter = new MyrecipeAdapter(this, R.layout.myrecipe_item, postingFormList, imgList);
+        MyrecipeAdapter adapter = new MyrecipeAdapter(this, R.layout.myrecipe_item, postingFormList, imgList2);
 
         listView.setAdapter(adapter);
 
@@ -115,7 +122,7 @@ public class ChristmasActivity extends AppCompatActivity implements View.OnClick
                 System.out.println("111111111112222222222222222333333333333344444444444444444444444444444444");
 
                 /* putExtra의 첫 값은 식별 태그, 뒤에는 다음 화면에 넘길 값 */
-                intent.putExtra("imgURL", imgList.get(position));
+                intent.putExtra("imgURL", imgList2.get(position));
                 intent.putExtra("title", postingFormList.get(position).getTitle());
                 intent.putExtra("description", postingFormList.get(position).getDescription());
                 intent.putExtra("information", postingFormList.get(position).getInformation());
