f6e56820552348b4522dd959af3c221cdbd1c8f0
Joe2357 <50836915+Joe2357@users.noreply.github.com>
2021-05-31 22:11:43 +0900

    지도 업로드

--- app/src/main/java/gachon/mpclass/mp_team_newnew/api/MapsActivity.java
+++ app/src/main/java/gachon/mpclass/mp_team_newnew/api/MapsActivity.java
@@ -2,7 +2,13 @@ package gachon.mpclass.mp_team_newnew.api;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,16 +17,31 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import gachon.mpclass.mp_team_newnew.R;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Geocoder geocoder;
+    private Button searchButton;
+    private Button okButton;
+    private EditText editText;
+    private String inputLine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_googlemap);
+
+        geocoder = new Geocoder(this, Locale.forLanguageTag("ko"));
+        searchButton = findViewById(R.id.btn_search);
+        okButton = findViewById(R.id.btn_ok);
+        editText = findViewById(R.id.editText);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -47,5 +68,25 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .position(sydney)
                 .title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputLine = editText.getText().toString();
+                List<Address> addressList = null;
+                try {
+                    addressList = geocoder.getFromLocationName(
+                            inputLine,
+                            10);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                String[] splitStr = addressList.get(0).toString().split(",");
+                String address = splitStr[0].substring(splitStr[0].indexOf("\"") + 1, splitStr[0].length() - 2); // 주소
+                Log.d("Position", address);
+            }
+        });
     }
 }
